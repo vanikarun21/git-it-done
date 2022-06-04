ef26b38c5f0330a96aa2e640c9e2e391ade4af78
Vani Karun <vanikarun21@yahoo.com>
2022-06-04 13:22:56 -0500

    added additional functions for the variable

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,17 @@
 
+// var getUserRepos = function() {
+//   console.log("function was called");
+// };
 var getUserRepos = function() {
-  console.log("function was called");
-};
+  fetch("https://api.github.com/users/octocat/repos");
+  var response = fetch("https://api.github.com/users/octocat/repos");
+console.log(response);
+fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+  response.json().then(function(data) {
+    console.log(data);
+  });
+});
 
+console.log("outside");
 getUserRepos();
 
