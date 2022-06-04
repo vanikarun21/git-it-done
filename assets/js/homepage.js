ee09c12d6c4b6c4e9279373a1b77c38626e26a3c
Vani Karun <vanikarun21@yahoo.com>
2022-06-04 13:25:42 -0500

    new codes for fetch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,15 +2,17 @@
 // var getUserRepos = function() {
 //   console.log("function was called");
 // };
-var getUserRepos = function() {
-  fetch("https://api.github.com/users/octocat/repos");
-  var response = fetch("https://api.github.com/users/octocat/repos");
-console.log(response);
-fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-  response.json().then(function(data) {
-    console.log(data);
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
   });
-});
+};
 
 console.log("outside");
 getUserRepos();
